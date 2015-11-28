fbcce45081eb4df4a191b7b8a085de3a25c2d915
Peter <Peter@users.noreply.github.com>
2015-11-29 04:40:02 +0800

    aasasasasas

--- 1.js
+++ 1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var $math = require('E:\\9F\\TEST\\__express\\express\\app-canvas\\libs\\math.js')
+var $math = require('E:\\9F\\TEST\\__express\\express\\app-canvas\\libs\\math.js');
 
 console.log(1);
 
@@ -8,3 +8,5 @@ var one = $math.cosD(30);
 console.log(one);
 
 console.log('hello');
+
+console.log('aaaa');
